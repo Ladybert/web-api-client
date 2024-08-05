b32a7b97388912b7024f90c6832ad79518cc1d0e
Ladybert <cyberrashad@gmail.com>
2024-08-05 12:48:00 +0700

    change protected

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bef8000c700932fb71e6603888ef1a5ce08a0dd89087b94e0d9b61da4b01d9a2
-size 1404
+oid sha256:012203741e75d1b25cf5229ab18f2c4170077f0083e8e0fb5bb5d5b848d3110a
+size 1405
