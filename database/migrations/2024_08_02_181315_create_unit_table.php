8a15ddc44a9ea13b59738d4430779f6ebbd5c7b9
Ladybert <cyberrashad@gmail.com>
2024-08-05 13:11:45 +0700

    change protected tb #2

--- database/migrations/2024_08_02_181315_create_unit_table.php
+++ database/migrations/2024_08_02_181315_create_unit_table.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b17c6fa26c0608411fc9796b9dc4242375fcd42e77f4e4af3749591fc5db520d
-size 1345
+oid sha256:5da62d871a63de5139ec153f3665acc24dc9e848dfd43e172cd6df9f8011ff08
+size 1344
