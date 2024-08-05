68e88c79c991c57706916c3ef238e7dce93eb225
Ladybert <cyberrashad@gmail.com>
2024-08-05 13:11:45 +0700

    change protected tb #2

--- database/migrations/2024_08_02_181315_create_unit_table.php
+++ database/migrations/2024_08_02_181315_create_unit_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('units', function (Blueprint $table) {
+        Schema::create('unit', function (Blueprint $table) {
             $table->id();
             $table->string('image');
             $table->string('name');
