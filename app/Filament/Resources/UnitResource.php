50e03a4312ba33f0731cfb354932d341e6f65974
Ladybert <cyberrashad@gmail.com>
2024-09-06 09:38:33 +0700

    make unit tabel & form

--- app/Filament/Resources/UnitResource.php
+++ app/Filament/Resources/UnitResource.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b79fc27af7937d3e72b3ca9bfbe8aa165515a2b8b5e1380d6fa64f1a374d4c9
-size 5861
+oid sha256:4f8c36745980fc2a8566d2f1a0ddec794ba35ee339a95567ff5f2c9e40d99c06
+size 6423
