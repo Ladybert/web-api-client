e8e61bef3f36375ed65a44c993b062d76163ed55
Ladybert <cyberrashad@gmail.com>
2024-09-06 10:27:52 +0700

    add new feat thumbnail & area building size

--- app/Filament/Resources/UnitResource.php
+++ app/Filament/Resources/UnitResource.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f8c36745980fc2a8566d2f1a0ddec794ba35ee339a95567ff5f2c9e40d99c06
-size 6423
+oid sha256:dc8ba26d8807e8c33844136cf60fedb7775b8ec19cdce791943c766ef8b31e48
+size 6583
