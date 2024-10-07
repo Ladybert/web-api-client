35b7a56ef1ba2e9d836203ff9c0b0c96f1a7e0db
Humdiess <hmzinchannel@gmail.com>
2024-10-07 12:54:40 +0700

    style : adjust footer layout

--- resources/views/page-layout/best-project.blade.php
+++ resources/views/page-layout/best-project.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cce3d14d4e16d1eba9bcbe045885b913bf294b915a7c8e49a1f4098d59ec2694
-size 8250
+oid sha256:4041203aa39a62b914a054355ba81343357088ed1c33b18f14e7c4077983825d
+size 8197
--- resources/views/page-layout/footer.blade.php
+++ resources/views/page-layout/footer.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78b91d5cfca9229e8d2a7ed602c0f9fd1e6614797e4b2a0cd0f4b243e1ae43ea
-size 1097
+oid sha256:e3590ce0db135f0e1539f835773f8c7cd5889f97238a74132e308a2455ff6419
+size 1071
