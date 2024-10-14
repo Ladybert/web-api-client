6094f861b07c44e92b8018d8451bc239c33b32ba
Humdiess <hmzinchannel@gmail.com>
2024-10-14 23:33:17 +0700

    style: adjust input search width

--- resources/views/housing/index.blade.php
+++ resources/views/housing/index.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9088da1fdc0584695c22b5f8c900bfb53703a8249ad8226090432339ae027d24
-size 3968
+oid sha256:ac1b332d03b36eea8c32b939f6738332bad0d002a1c833807f798aa2b2fe110a
+size 3981
