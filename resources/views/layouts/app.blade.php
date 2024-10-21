8f5fa3238a4020f7eae96d7edc14307aa4f482cb
Humdiess <hmzinchannel@gmail.com>
2024-10-21 13:32:29 +0700

    style: resposive mobile menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5962386e505bd38c32c0ba8389d2654b67e57b9bdcb5401704db69261a1f6c93
-size 5713
+oid sha256:e96ee6c3aaafc7de3d2fecce33930b42fcd0d9683bb2fe9da6261fd336ee85a2
+size 5723
