60e5d4e08a8911a1373f14a3af2e4e8b6f073f88
Humdiess <hmzinchannel@gmail.com>
2024-10-29 16:22:23 +0700

    feat: sign in modal

--- resources/views/components/layouts/app-layout.blade.php
+++ resources/views/components/layouts/app-layout.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1771689816f9473208dcf94056bfc288eedba18f853c6fdd63339743daaf2675
-size 2296
+oid sha256:7cd063dea9b45febbe73d1fdd4442a064ddde1132b2b62e91d4032a51e7bdb92
+size 3081
--- resources/views/components/layouts/app-navbar.blade.php
+++ resources/views/components/layouts/app-navbar.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b69bc1bf336bae791c6afad24a53c61a16e5aabcbaf73fcb924d7e4bcca9bbb
-size 1336
+oid sha256:1186849d722bcfdd96ab0184df9167ed50b2a678496a03f87e23812e9254e0b5
+size 1347
